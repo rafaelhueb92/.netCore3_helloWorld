dbce3127fd35c81f88913da6d6e6477ba5714665
rafgahu <rafaelgabrielhueb@correio.itau.com.br>
2020-02-17 09:45:30 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace helloWorld {
+    class Program {
+        static void Main (string[] args) {
+            var myclass = new MyClass();
+            Console.WriteLine ($"Hello World! {myclass.ReturnMessage()}");
+        }
+    }
+}
